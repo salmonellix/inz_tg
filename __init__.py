892e9472b5e55cddb015700efadc96745607d37a
salmonellix <44066048+salmonellix@users.noreply.github.com>
2020-07-06 19:53:39 +0200

    Best_version

--- __init__.py
+++ __init__.py
@@ -1,22 +1,18 @@
 
+
 from __future__ import absolute_import
 from __future__ import print_function
 
-
 # Avoid importing "expensive" modules here (e.g. scipy), since this code is
 # executed on PyMOL's startup. Only import such modules inside functions.
-
-import os
+from typing import List, Any
 
 from PyQt5.QtWidgets import QFileDialog
 
 
 def __init_plugin__(app=None):
-    '''
-    Add an entry to the PyMOL "Plugin" menu
-    '''
     from pymol.plugins import addmenuitemqt
-    addmenuitemqt('hott', run_plugin_gui)
+    addmenuitemqt('HS-Detector', run_plugin_gui)
 
 
 # global reference to avoid garbage collection of our dialog
@@ -24,9 +20,6 @@ dialog = None
 
 
 def run_plugin_gui():
-    '''
-    Open our custom dialog
-    '''
     global dialog
     if dialog is None:
         dialog = make_dialog()
@@ -45,17 +38,14 @@ def make_dialog():
     from PyQt5 import QtGui
     import os
     from pymol import cmd, stored
-
     import gzip
     import json
     import random
     import string
-
     from numpy import log
     import numpy as np
     import copy
-
-    import sys
+    import collections
     # create a new Window
     dialog = QtWidgets.QDialog()
     QtWidgets.QApplication.setStyle(QtWidgets.QStyleFactory.create('Fusion'))
@@ -63,91 +53,107 @@ def make_dialog():
     uifile = os.path.join(os.path.dirname(__file__), 'gui_alfa3.ui')
     form = loadUi(uifile, dialog)
 
+    # global variables
+    Hs_files = []
+    Proteines_files = []
+    Hs_paths = []
+    Proteines_paths = []
+    Jsons_paths = []
+    solvent = 'non'
 
-    hs_files=[]
-    proteines_files=[]
-    hs_paths = []
-    proteines_paths = []
-    jsons_paths=[]
-
-
+    # load protein with hot spots and json file with reference values
     def browse_filename_hs_protein():
+        # load protein
         protein_path = QFileDialog.getOpenFileName(form, "Open File", "C:\\User\\", 'PDB File (*.pdb)')
-        hs_path = QFileDialog.getOpenFileName(form, "Open File", "C:\\User\\", 'MOL2 File (*.mol2)')
-        hs_path=hs_path[0]
-        hs_paths.append(hs_path)
-        hs_path=hs_path.replace(os.path.sep, "\\")
-
         protein_path = protein_path[0]
-        proteines_paths.append(protein_path)
+        Proteines_paths.append(protein_path)
         protein_path = protein_path.replace(os.path.sep, "\\")
-        jsons_path=QFileDialog.getOpenFileName(form, "Open File", "C:\\User\\", 'json File (*.json)')
+        # load hs file
+        hs_path = QFileDialog.getOpenFileName(form, "Open File", "C:\\User\\", 'MOL2 File (*.mol2)')
+        hs_path = hs_path[0]
+        Hs_paths.append(hs_path)
+        hs_path = hs_path.replace(os.path.sep, "\\")
+        # load json
+        jsons_path = QFileDialog.getOpenFileName(form, "Open File", "C:\\User\\", 'json File (*.json)')
         jsons_path = jsons_path[0]
-        jsons_paths.append(jsons_path)
+        Jsons_paths.append(jsons_path)
 
         if hs_path:
-            #cmd.load(hs_path)
             hs_name = os.path.basename(os.path.normpath(hs_path))
             hs_name = os.path.splitext(hs_name)[0]
-            hs_files.append(hs_name)
-            form.browser_files1.setText('\n '.join(hs_files))
+            if hs_name != ' ':
+                Hs_files.append(hs_name)
+                form.browser_files1.setText('\n '.join(Hs_files))
 
-            #cmd.load(protein_path)
             protein_name = os.path.basename(os.path.normpath(protein_path))
             protein_name = os.path.splitext(protein_name)[0]
-            proteines_files.append(protein_name)
-            form.browser_files1_2.setText('\n '.join(proteines_files))
+            if protein_name != ' ':
+                Proteines_files.append(protein_name)
+                form.browser_files1_2.setText('\n '.join(Proteines_files))
 
-
-
-
-    #load files that are browsed
+    # load files that are browsed
     def load_hs_protein():
-        if len(hs_files) == len(proteines_files):
+        if len(Hs_files) == len(Proteines_files):
             cmd.delete('all')
-            for nbr in range(0,len(hs_files)):
-                cmd.load(hs_paths[nbr])
-                cmd.select(hs_files[nbr])
+            for nbr in range(0, len(Hs_files)):
+                cmd.load(Hs_paths[nbr])
+                cmd.select(Hs_files[nbr])
+                cmd.load(Proteines_paths[nbr])
                 rezise(nbr)
-                cmd.load(proteines_paths[nbr])
-            cmd.extra_fit(','.join(hs_files))
-            cmd.extra_fit(','.join(proteines_files))
+            try:
+                cmd.extra_fit(','.join(Hs_files))
+                cmd.extra_fit(','.join(Proteines_files))
+            except:
+                form.browser_files1.setText('ERROR -_-')
+                form.browser_files1_2.setText('Number of HotSpots and Structures must be the same!!!')
 
         else:
-            form.browser_files1.setText(', '.join('ERROR -_-'))
+            form.browser_files1.setText('ERROR      -_-')
             form.browser_files1_2.setText('Number of HotSpots and Structures must be the same!!!')
 
-    # change size of hs depends on reference_density_correction from .json file
+    # change size of hs depends on reference_density_correction from .json file (one fil for one pond calculations)
     def rezise(nbrs):
-        def _sele_exists(sele):
+        def is_sele(sele):
             return sele in cmd.get_names('all')
 
         def _random_string(n=10):
             return ''.join(random.choice(string.ascii_uppercase + string.digits) for _ in range(n))
 
+        refs = []
+        ref = 1
+        for i in range(0, len(Jsons_paths)):
+            with gzip.open(Jsons_paths[i]) as f:
+                refOne = float(json.load(f)["reference_density_correction"])
+                refs.append(refOne)
+
+        if form.oneSolvent.isChecked():
+            ref = np.median(refs)
+        if form.multipleSolvent.isChecked():
+            with gzip.open(Jsons_paths[nbrs]) as f:
+                ref = float(json.load(f)["reference_density_correction"])
+        else:
+            ref = np.median(refs)
+
         def hs_resize(meta_file, selection):
 
-            # Changes size of hotspots depending on normalized partial_charge values.
-
-            if not _sele_exists(selection):
+            if not is_sele(selection):
                 raise RuntimeError("Selection \"{}\" does not exists.".format(selection))
 
             # Find free sele name
             temp_sele = _random_string()
-            while _sele_exists(temp_sele):
+            while is_sele(temp_sele):
                 temp_sele = _random_string()
 
             states = cmd.count_states(selection=selection)
-
-            with gzip.open(meta_file) as f:
-                ref = float(json.load(f)["reference_density_correction"])
 
             for state in range(1, states + 1):
                 stored.info = []
                 cmd.iterate_state(state, selection, "stored.info.append((ID, partial_charge))")
 
                 for id_, partial_charge in stored.info:
-                    size = log(partial_charge / ref * 1. + 1)
+                    size = log(partial_charge / ref + 1)
+                    print(ref)
+                    print(size)
 
                     cmd.select(temp_sele, "{} and id {}".format(selection, id_), state=state)
                     cmd.set("sphere_scale", value=size, selection=temp_sele)
@@ -155,22 +161,23 @@ def make_dialog():
 
             cmd.delete(temp_sele)
 
-        hs_resize(jsons_paths[nbrs],hs_files[nbrs])
+        hs_resize(Jsons_paths[nbrs], Hs_files[nbrs])
 
-
-    # save all selected hot spots
+    # save all hs into one mol2 file
     def save_all_hs():
-        cmd.select('sele','resn FIL')
-        file_name=QFileDialog.getSaveFileName(form, 'Save File','', 'MOL2 File (*.mol2)')
-        file_name=file_name[0]
-        cmd.extract('all_FIL','sele')
-        cmd.save(file_name, 'all_FIL')
+        cmd.select('sele', 'resn FIL')
+        file_name = QFileDialog.getSaveFileName(form, 'Save File', '', 'MOL2 File (*.mol2)')
+        file_name = file_name[0]
+        cmd.create('all_hs', 'sele')
+        cmd.save(file_name, 'all_hs', 0)
+        cmd.delete('all_hs')
 
-
-
-    # group hot spots and show only representation
+    # simplification geometric centre / hs weights
     def hs_sipmly():
-        def _sele_exists(sele):
+
+        cmd.delete('simply')
+
+        def is_sele(sele):
             return sele in cmd.get_names("all")
 
         def find_all_coords(n, hs_id, exc_indexes=[], coords=[]):
@@ -183,6 +190,7 @@ def make_dialog():
             return exc_indexes, coords
 
         def free_hotspot_id(hs, exc_indexes):
+
             for h in hs:
                 if (h[0], h[3]) not in exc_indexes:
                     return h[0], h[3]
@@ -197,24 +205,53 @@ def make_dialog():
 
             return sum_x / length, sum_y / length, sum_z / length
 
-        def find_center(*args, **kwargs):
+        def find_center(partial_charges, coords, nbr):
+            dict_value_coords = {}
+            coords_weight=[]
+
+            for p, c in zip(partial_charges,coords):
+                dict_value_coords[p] = c
+            od_dict = collections.OrderedDict(sorted(dict_value_coords.items(), reverse=True))
+            partial_charges=list(od_dict.keys())
+            nbr=len(partial_charges)-nbr
+
+            for i in range(0,nbr):
+                try:
+                    max_hs = partial_charges[i]
+                    x = od_dict[max_hs][0]
+                    y = od_dict[max_hs][1]
+                    z = od_dict[max_hs][2]
+                    coords_weight.append([x, y, z])
+                except:
+                    pass
+            return coords_weight
+
+
+        def hs_gsimplifier(*args, **kwargs):
+
             radius = form.radius1.value()
             color = kwargs.get("color", "red")
 
             selections = args
 
             for selection in selections:
-                if not _sele_exists(selection):
+                if not is_sele(selection):
                     raise RuntimeError("Selection \"{}\" does not exists.".format(selection))
 
             states = cmd.count_states(selection=selections[0])
-
+            hs_values=[]
+            all_coords=[]
             for state in range(1, states + 1):
                 stored.info = []
 
                 for selection in selections:
                     cmd.iterate_state(state, selection,
                                       "stored.info.append([ID, partial_charge, (x,y,z), \"{}\"])".format(selection))
+                for line in stored.info:
+                    partial_charge = line[1]
+                    coord = line[2]
+                    hs_values.append(partial_charge)
+                    all_coords.append(coord)
 
                 hs = np.array(copy.copy(stored.info))
 
@@ -226,86 +263,90 @@ def make_dialog():
 
                 exc_indexes = []
                 hs_id = free_hotspot_id(hs, exc_indexes=exc_indexes)
+
                 while hs_id is not None:
                     excluded, coords = find_all_coords(n, hs_id, exc_indexes=[], coords=[])
                     exc_indexes.extend(excluded)
-
-                    middle_point = group_center(np.array(coords))
-                    cmd.pseudoatom(object="simply",
-                                   color=color,
-                                   pos=middle_point,
-                                   state=state)
+                    if form.geometric.isChecked():
+                        middle_point = group_center(np.array(coords))
+                        cmd.pseudoatom(object="simply",
+                                       color=color,
+                                       pos=middle_point,
+                                       state=state)
+                    else:
+                        middle_points = find_center(hs_values, all_coords, nbr=len(exc_indexes))
+                        for point in middle_points:
+                            cmd.pseudoatom(object="simply",
+                                           color=color,
+                                           pos=point,
+                                           state=state)
 
                     hs_id = free_hotspot_id(hs, exc_indexes=exc_indexes)
 
                 cmd.show(representation="spheres", selection="simply")
 
-        find_center('all_FIL', color = 'magenta')
+        cmd.select('hs2', 'resn FIL')
+        hs_gsimplifier('hs2', color='hotpink')
 
-    # save simplyfied groups of hot spots
     def save_simply():
         cmd.select('simply')
-        file_name=QFileDialog.getSaveFileName(form, 'Save File','', 'MOL2 File (*.mol2)')
-        file_name=file_name[0]
+        file_name = QFileDialog.getSaveFileName(form, 'Save File', '', 'MOL2 File (*.mol2)')
+        file_name = file_name[0]
         cmd.save(file_name, 'simply')
 
-    # callback for the "Browse" button PDB files
-    str_files=[]
-    def browse_filename_pdb():
-        filepath =QFileDialog.getOpenFileName(form, "Open File", "C:\\User\\",'PDB File (*.pdb)')
-        filepath=filepath[0]
-        filepath=filepath.replace(os.path.sep, "\\")
-        if filepath:
-            file_name = os.path.basename(os.path.normpath(filepath))
-            file_name = os.path.splitext(file_name)[0]
-            str_files.append(file_name)
-            cmd.load(filepath)
-            print(filepath)
-
-    #align structures
-    def align_str():
-        print(str_files)
-        cmd.extra_fit('\n'.join(str_files))
-
-    #make hs color
+    # make hs color
     def color_hs():
-        try:
-            cmd.spectrum("pc", "blue_white_red",'all_FIL')
-            #cmd.spectrum("pc", "blue_white_red", 'hotspot'+'*')
-        except:
-            cmd.spectrum("pc", "blue_white_red", 'hotspot')
-            cmd.spectrum("pc", "blue_white_red", 'hs'+'*')
+        color = form.colors.currentItem()
+        color = str(color.text())
 
-    picked_hs=0
-    def choose_hs():
-        picked_hs=cmd.count_atoms('sele')
-        print(picked_hs)
-        #picked_hs.append(hs_names)
+        try:
+            cmd.spectrum("pc", ('%s' % color), 'sele')
+
+        except:
+            cmd.spectrum("pc", ('%s' % color), 'hotspot')
+            cmd.spectrum("pc", '%s' % color, 'hs' + '*')
+
+
+
+    def choose_hs(picked_hs = 0):
+        picked_hs = cmd.count_atoms('sele')
         form.hs_browse.setText(str(picked_hs))
-        cmd.extract('sele', 'sele')
+        cmd.create('hs', 'sele')
 
     def clear_pick_hs():
-        picked_hs=0
-        form.hs_browse.setText(', '.join(picked_hs))
-
-
+        picked_hs = 0
+        form.hs_browse.setText(str(picked_hs))
 
     def clean_all():
         cmd.delete('all')
-        hs_files.clear()
-        proteines_files.clear()
-        hs_paths.clear()
-        proteines_paths.clear()
-        jsons_paths.clear()
-        form.browser_files1_2.setText(', '.join(proteines_files))
-        form.browser_files1.setText(', '.join(proteines_files))
+        Hs_files.clear()
+        Proteines_files.clear()
+        Hs_paths.clear()
+        Proteines_paths.clear()
+        Jsons_paths.clear()
+        form.browser_files1_2.setText(', '.join(Proteines_files))
+        form.browser_files1.setText(', '.join(Proteines_files))
+        form.hs_browse.setText(' ')
 
+    def solvent_one():
+        global solvent
+        solvent = 'one'
 
+    def solvent_multiple():
+        global solvent
+        solvent = 'multiple'
 
+    def search_groups():
+        radius2 = form.radius2.value()
+        cmd.select('sele_atms', 'polymer.protein within %f of hs' %(radius2))
 
+    def save_atms():
+        cmd.select('sele_atms')
+        file_name = QFileDialog.getSaveFileName(form, 'Save File', '', 'PDB File (*.pdb)')
+        file_name = file_name[0]
+        cmd.save(file_name, 'sele_atms', -1)
 
     # hook up button callbacks
-    # form.button_ray.clicked.connect(run)
     form.browse_button1.clicked.connect(browse_filename_hs_protein)
     form.load1.clicked.connect(load_hs_protein)
     form.button_close.clicked.connect(dialog.close)
@@ -317,4 +358,8 @@ def make_dialog():
     form.save_file2_2.clicked.connect(save_simply)
     form.add_hs.clicked.connect(choose_hs)
     form.clear_hs.clicked.connect(clear_pick_hs)
+    form.oneSolvent.clicked.connect(solvent_one)
+    form.multipleSolvent.clicked.connect(solvent_multiple)
+    form.ok2.clicked.connect(search_groups)
+    form.save_file3.clicked.connect(save_atms)
     return dialog
